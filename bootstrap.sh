10860763698aee07b339a5d9767cd8685eda1ae4
binaryninja <Richarjb@gmail.com>
2015-06-13 17:13:13 +0000

    Auto install capstone, ROPgadgets, and binwalk

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+adduser student
+adduser student sudo
+pip install capstone
+git clone https://github.com/binaryninja/ROPgadget.git
+git clone https://github.com/devttys0/binwalk.git
+cd binwalk
+./deps.sh
+python setup.py install
+
