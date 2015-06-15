3c9fe336d706a9176b98aea4e18056a50cf7593b
Jeremy Richards <richarjb@gmail.com>
2015-06-14 23:28:40 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 adduser student
 adduser student sudo
 pip install capstone
+apt-get install sigrok
 git clone https://github.com/binaryninja/ROPgadget.git
 git clone https://github.com/devttys0/binwalk.git
 cd binwalk
