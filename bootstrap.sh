96b85d1ce7967f21d11fd551b6dcac9cd2f407db
Jeremy Richards <richarjb@gmail.com>
2015-06-19 13:34:40 -0400

    Update bootstrap.sh
    Disable ASLR for exploit dev
    Enable core dumps

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,11 @@ apt-get upgrade
 pip install capstone
 apt-get install sigrok
 
+echo 0 > /proc/sys/kernel/randomize_va_space
+
+ulimit -c unlimited
+
+
 adduser student
 adduser student sudo
 cd /home/student
