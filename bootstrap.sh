4b86101f1a2fb5be5f506c9dab036a85b69420c1
Jeremy Richards <richarjb@gmail.com>
2015-08-15 10:44:08 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-mv led_aging.sh /etc/init.d/led_aging.sh
+mv fix/ledfix.sh /etc/init.d/led_aging.sh
 
 apt-get -y update
 apt-get -y upgrade
@@ -10,28 +10,27 @@ echo 0 > /proc/sys/kernel/randomize_va_space
 
 ulimit -c unlimited
 
-
-adduser student
-adduser student sudo
-cd /home/student
-
 git clone https://github.com/binaryninja/ROPgadget.git
 git clone https://github.com/devttys0/binwalk.git
 cd binwalk
-yes | ./deps.sh
+printf 'y' | ./deps.sh
 python setup.py install
 cd ..
 
 svn co svn://flashrom.org/flashrom/trunk flashrom
 cd flashrom ; ./configure ; make ; make install ; cd ..
 
+
 apt-get -y remove automake1.9
 apt-get -y install automake1.10
 
-#urJtag svn broken
+#urJtag svn broken/doesnt compile... using stable
 #svn co http://svn.code.sf.net/p/urjtag/svn/trunk urjtag
 #cd urjtag/urjtag/ ; ./autogen.sh ; make ; make install ; cd ..
 
 wget http://sourceforge.net/projects/urjtag/files/urjtag/0.10/urjtag-0.10.tar.gz/download
 tar -zxvf download ; cd urjtag-0.10/ ; ./configure ; make ; make install ; cd ..
 
+adduser student
+adduser student sudo
+cd /home/student
